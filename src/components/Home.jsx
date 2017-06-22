48ab44b0f9f997dff1cf088337c1ead69a43b0de
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-06-21 23:29:28 -0700

    responsive design with mobile first

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = (props) => {
     return (
       <li
         onClick={props.shelfActions.addToCart.bind(this, item)}
-        key={i} className="col-sm-1 list-item-small">
+        key={i} className="col-xs-3 col-sm-2 col-md-1 list-item-small">
         <img
           className="img img-responsive"
           src={item.src}
@@ -27,7 +27,7 @@ const Home = (props) => {
     return (
       <li
         key={i} className="col-sm-4 list-item-big">
-        <div className="container-thirds">
+        <div className="container-thirds overflow-scroll">
           <div className="col-sm-12">
             <img
               className="inline float-left img-responsive padding-right-1"
