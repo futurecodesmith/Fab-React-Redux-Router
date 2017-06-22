aa239ab73af69a1e04eea718d965bb5ae4dfee38
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-06-22 11:29:22 -0700

    add fade in transition

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,7 @@ const Login = (props) => {
   return (
     <div>
       <Navbar />
-      <div className="container-fluid">
+      <div className="container-fluid fade-in">
         <div className="row">
           <div className="col-sm-10 col-sm-offset-1">
             <Form />
