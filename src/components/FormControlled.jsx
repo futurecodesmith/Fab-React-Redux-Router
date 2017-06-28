aa839852a2829b424a4434a445034d5c16c1c5fe
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-06-28 12:31:20 -0700

    add controlled form

---
+++ src/components/FormControlled.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class FormControlled extends Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    name: '',
+    password: ''
+  }
+
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(this.state.name, this.state.password);
+    this.setState({
+      name: '',
+      password: '',
+    })
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div className="center form">
+        <form onSubmit={this.handleSubmit.bind(this)} onChange={this.handleChange.bind(this)}>
+          <input className="input block center" type="text" placeholder="Name" value={this.state.name} name="name" />
+          <input className="input block center" type="password" name="password" placeholder="Password" value={this.state.password} />
+          <input className="btn input-button" type="submit" value="Login" />
+        </form>
+      </div>
+    )
+  }
+
+}
+
+  export default FormControlled;
